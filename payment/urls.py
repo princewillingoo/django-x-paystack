ad15ae4a1f161788a34d555a5a0cc97598569d45
princewilling <princewillingoo@gmail.com>
2023-03-03 18:43:12 +0100

    Integrating Paystack Checkout

--- payment/urls.py
+++ payment/urls.py
@@ -5,4 +5,7 @@ app_name = 'payment'
 
 urlpatterns = [
     path('', views.payment_init, name='create'),
+    path('process/', views.payment_process, name='process'), #new
+    path('success/', views.payment_success, name='success'), #new
+    path('canceled/', views.payment_canceled, name='canceled'), #new
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,16 @@
+import json
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.conf import settings
 from django.urls import reverse
+import requests
 from .forms import PaymentInitForm
+from django.shortcuts import get_object_or_404
+from payment.models import Payment
+
+# create the Paystack instance
+api_key = settings.PAYSTACK_TEST_SECRETE_KEY
+url = settings.PAYSTACK_INITIALIZE_PAYMENT_URL
 
 def payment_init(request):
     if request.method == 'POST':
@@ -22,3 +31,56 @@ def payment_init(request):
     # render form if GET request
         form = PaymentInitForm()
     return render(request, 'payment/create.html', {'form': form})
+
+
+def payment_process(request):
+    # retrive the payment_id we'd set in the djago session ealier
+    payment_id = request.session.get('payment_id', None)
+    # using the payment_id, get the database object
+    payment = get_object_or_404(Payment, id=payment_id)
+    # retrive payment amount 
+    amount = payment.get_amount()
+
+    if request.method == 'POST':
+        success_url = request.build_absolute_uri(
+            reverse('payment:success'))
+        cancel_url = request.build_absolute_uri(
+            reverse('payment:canceled'))
+        
+        # metadata to pass additional data that 
+        # the endpoint doesn't accept naturally.
+        metadata= json.dumps({"payment_id":payment_id,  
+                              "cancel_action":cancel_url,   
+                            })
+
+        # Paystack checkout session data
+        session_data = {
+            'email': payment.email,
+            'amount': int(amount),
+            'callback_url': success_url,
+            'metadata': metadata
+            }
+                
+        headers = {"authorization": f"Bearer {api_key}"}
+        # API request to paystack server
+        r = requests.post(url, headers=headers, data=session_data)
+        response = r.json()
+        if response["status"] == True :
+            # redirect to Paystack payment form
+            try:
+                redirect_url = response["data"]["authorization_url"]
+                return redirect(redirect_url, code=303)
+            except:
+                pass
+        else:
+            return render(request, 'payment/process.html', locals())
+    else:
+        return render(request, 'payment/process.html', locals())
+    
+###previous code###
+
+def payment_success(request):
+    return render(request, 'payment/success.html')
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
